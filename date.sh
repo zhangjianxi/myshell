cf8827ff3e300d6abb88b00268b94b4606993b57
jianxi <jianxi@163.com>
2013-03-14 02:30:16 +0000

    date.sh

---
+++ date.sh
@@ -0,0 +1,30 @@
+#!\bin\bash
+
+RES=1
+RES2=2
+WHILE_FLAG=0
+SYS_1=`date +%s`
+echo $SYS_1
+
+sleep 10
+
+SYS_2=`date +%s`
+echo $SYS_2
+
+INTERVAL=`expr $SYS_2 - $SYS_1`
+
+echo $INTERVAL
+
+while [ ${RES} -eq 1 ] && [ ${WHILE_FLAG} -le 3 ]
+	do
+		echo "111"
+		if [ ${INTERVAL} -le 11 ]
+			then
+				let WHILE_FLAG++
+				echo ${WHILE_FLAG}
+		else
+			WHILE_FLAG=0
+		fi
+		done
+
+
